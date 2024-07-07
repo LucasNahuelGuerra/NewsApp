490c90f226cbea8bf255c04ebb425a47ece0d46e
Lucas Nahuel Guerra <lucasnahuelguerra@icloud.com>
2024-07-07 11:27:54 -0300

    Initial Commit

---
+++ NewsApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewsApp
+//
+//  Created by Lucas Nahuel Guerra on 07/07/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
